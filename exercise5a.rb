2ca63313755f048ac6793730a1f166bf46f025f4
Richard-Howard-Toronto <rh_gk@rogers.com>
2017-09-18 15:49:52 -0400

    this version works, so now I will try and break it

--- exercise5a.rb
+++ exercise5a.rb
@@ -15,25 +15,34 @@ while distance_from_house_var > 0
   puts "would you like to walk or run?"
   response_var = gets.chomp.to_s
 
-if response_var == "walk"
-  puts "you are walking"
-  distance_from_house_var -= walk_speed
-  puts "distance from house is now #{distance_from_house_var}"
+  if distance_from_house_var < run_speed
 
-elsif response_var == "run"
-  puts "you are running"
-  distance_from_house_var -= run_speed
-  puts "distance from house is now #{distance_from_house_var}"
+      puts "you are less than #{run_speed} distance from house so you end up walking the last mile and your distance is zero, you are home !"
 
-else
-  puts "you did not enter walk or run"
+  else
 
-end #if
+    if response_var == "walk"
+      puts "you are walking"
+      distance_from_house_var -= walk_speed
+      puts "distance from house is now #{distance_from_house_var}"
 
-end #while
+    elsif response_var == "run"
+      puts "you are running"
+      distance_from_house_var -= run_speed
+      puts "distance from house is now #{distance_from_house_var}"
 
+    else
+      puts "you did not enter walk or run"
 
-puts "info on the class ----------------------"
+    end #if
+
+    end #while
+
+  end #if
+
+puts "you are home!!!!!!!!"
+
+#puts "info on the class ----------------------"
 
 #puts "the response_var is a class of:"
 #puts response_var.class
