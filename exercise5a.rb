8e63ba6c2d43b919c9d428394a0df25c5c2c7dab
Richard-Howard-Toronto <rh_gk@rogers.com>
2017-09-18 15:11:46 -0400

    311

--- exercise5a.rb
+++ exercise5a.rb
@@ -1,4 +1,19 @@
-puts 1.is_a? Numeric
-"foo".is_a? String
-1.class
-"foo".class
+#puts 1.is_a? Numeric
+#puts "foo".is_a? String
+#puts 1.class
+#puts "foo".class
+#puts "----------------------------"
+
+# this is a program to measure the speed at which a person reached their house
+
+puts "would you like to walk or run?"
+
+response_var = gets.chomp.to_s
+
+if response_var = "walk"
+  puts "you are walking"
+elsif response_var = "run"
+  puts "you are running"
+else
+  puts "you are stopped"
+end
